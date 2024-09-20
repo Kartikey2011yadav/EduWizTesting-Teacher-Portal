b0a2b6c4d2da815b01f31c3a8c93a0f0e267b07d
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-09-20 21:04:58 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        {/* Define your routes */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;

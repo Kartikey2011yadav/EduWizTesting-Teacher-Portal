c19622534b26748f2897fec6974cf17246f7b080
RchtDshr <rachita17dashore@gmail.com>
2024-09-22 16:45:45 +0530

    schedule complete

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "../Sidebar/Sidebar";
 import ThemeToggleButton from "../Components/ThemeToggle";
+import Schedule from "../Schedule/Schedule";
 
 const Home = () => {
   return (
     <div>
       <Sidebar/>
+      <Schedule/>
       <div className=" absolute right-5 top-50 ">
         <ThemeToggleButton/>
       </div>

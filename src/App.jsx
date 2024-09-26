4c680ba980a6dd1dfd518e7cd8b27d5e8b223afe
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-09-26 23:25:42 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 import { initializeTheme } from './utils/theme.js';
 import Login from "./Login/Login.jsx";
 import SignUp from './SignUp/SignUp.jsx'
+import NotFound from "./NotFound/NotFound.jsx";
 
 
 
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/test" element={<Test />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );

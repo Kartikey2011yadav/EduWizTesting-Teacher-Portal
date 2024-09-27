c750d0948cd1d78a414a48dba914ab355f6bbfd4
Kartikey2011yadav <nykay10968@gmail.com>
2024-09-27 15:22:29 +0530

    test 4

--- src/Components/ThemeToggle.jsx
+++ src/Components/ThemeToggle.jsx
@@ -1,5 +1,4 @@
-
-import  { useContext } from 'react';
+import { useContext } from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
@@ -13,11 +12,9 @@ const ThemeToggleButton = () => {
       aria-label="Toggle Dark Mode"
     >
       <div className="flex items-center justify-between h-full px-1">
-      
-          <Sun className="w-5 h-5 text-yellow-500" />
-       
-          <Moon className="w-5 h-5 text-indigo-300" />
-       
+        <Sun className="w-5 h-5 text-yellow-500" />
+
+        <Moon className="w-5 h-5 text-indigo-300" />
       </div>
       <div
         className={`absolute top-0.5 left-0.5 w-7 h-7 rounded-full dark:bg-primary bg-white shadow-md transform transition-transform duration-200 ease-in-out ${

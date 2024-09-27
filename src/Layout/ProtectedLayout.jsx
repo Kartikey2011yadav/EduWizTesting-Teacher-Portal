8c7383f4cce68c3353580299fb5952e4980d4c99
Kartikey2011yadav <nykay10968@gmail.com>
2024-09-27 14:56:46 +0530

    test

--- src/Layout/ProtectedLayout.jsx
+++ src/Layout/ProtectedLayout.jsx
@@ -1,20 +1,28 @@
-
-import { Outlet } from "react-router-dom";
-import logo from "../assets/user photo default.jpg";
-import Sidebar from "../Sidebar/Sidebar";
-import TeacherNavbar from "../Navbar/TeacherNavbar";
+import { Outlet } from 'react-router-dom';
+import logo from '../assets/user photo default.jpg';
+import Sidebar from '../Sidebar/Sidebar';
+import TeacherNavbar from '../Navbar/TeacherNavbar';
 
 const ProtectedLayout = () => {
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex-1 flex flex-col">
-        <TeacherNavbar name={localStorage.getItem("name")} img={logo} />
+        <TeacherNavbar name={localStorage.getItem('name')} img={logo} />
         <div className="p-4">
           <Outlet />
         </div>
-      </div>
+      </div>   
     </div>
+
+
+
+
+
+
+
+
+
   );
 };
 
